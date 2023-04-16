d2f22d8a0d25ec149623219cc78fb6822cebfc49
Erik Golinelli <erik@codekraft.it>
2023-04-16 21:14:25 +0200

    time functions unit tests

--- tests/jsunit/main.test.js
+++ tests/jsunit/main.test.js
@@ -1,5 +1,6 @@
 /* JEST unit testing */
-import { nowInSeconds } from '../../src/scripts/utils';
+import { WEEK_IN_MILLISECONDS } from '../../src/scripts/constants';
+import { formatDate, nowInSeconds, splitByDate } from '../../src/scripts/utils';
 import { purify } from '../../src/scripts/utils/sanitization';
 
 describe( 'purify works', () => {
@@ -7,11 +8,56 @@ describe( 'purify works', () => {
 		expect( purify( '<div>sadsadsad</div>' ) ).toMatchObject( {
 			__html: '&lt;div>sadsadsad&lt;/div>',
 		} );
+		expect( purify( '<div>sadsadsad</div>' ) ).not.toMatchObject( {
+			__html: '<div>sadsadsad</div>',
+		} );
 	} );
 } );
 
 describe( 'time functions are working', () => {
-	it( 'do not break html', () => {
-		expect( nowInSeconds() ).toBeGreaterThan( 123 );
+	it( 'return a date', () => {
+		expect( nowInSeconds() ).toBeTruthy();
+	} );
+
+	it( 'return the correct day name', () => {
+		expect( formatDate( new Date() ) ).toContain(
+			new Date().toLocaleString( 'en-us', { weekday: 'long' } )
+		);
+	} );
+
+	it( 'can sort correctly the dates', () => {
+		expect(
+			splitByDate( [
+				{
+					date: new Date(),
+					title: 'new Date()',
+				},
+				{
+					date: new Date( Date.now() - WEEK_IN_MILLISECONDS * 0.002 ),
+					title: 'new Date( Date.now() - WEEK_IN_MILLISECONDS * 0.002 )',
+				},
+				{
+					date: new Date( Date.now() - WEEK_IN_MILLISECONDS * 2 ),
+					title: 'new Date( Date.now() - WEEK_IN_MILLISECONDS * 2 )',
+				},
+			] )
+		).toMatchObject( [
+			[
+				{
+					date: new Date(),
+					title: 'new Date()',
+				},
+				{
+					date: new Date( Date.now() - WEEK_IN_MILLISECONDS * 0.002 ),
+					title: 'new Date( Date.now() - WEEK_IN_MILLISECONDS * 0.002 )',
+				},
+			],
+			[
+				{
+					date: new Date( Date.now() - WEEK_IN_MILLISECONDS * 2 ),
+					title: 'new Date( Date.now() - WEEK_IN_MILLISECONDS * 2 )',
+				},
+			],
+		] );
 	} );
 } );
